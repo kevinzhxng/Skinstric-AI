4b30164b105956722bffea38c5522c8f257ba2cb
Kevin Zhang <kzhang0826@gmail.com>
2025-06-23 23:37:35 +0530

    gsap animations for homepage 80% done

--- app/page.js
+++ app/page.js
@@ -1,29 +1,278 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
+import gsap from "gsap";
+import { useRef } from "react";
 
 export default function Home() {
   const router = useRouter();
-  
+  const sophisticatedRef = useRef(null);
+  const skincareRef = useRef(null);
+  const leftGroupRef = useRef(null);
+  const rightGroupRef = useRef(null);
+
+  //diamond refs for animation
+  const leftOuterDiamondRef = useRef(null);
+  const leftInnerDiamondRef = useRef(null);
+  const rightOuterDiamondRef = useRef(null);
+  const rightInnerDiamondRef = useRef(null);
+
+  //button refs for animation
+  const leftInnerButtonRef = useRef(null);
+  const leftOuterButtonRef = useRef(null);
+  const rightInnerButtonRef = useRef(null);
+  const rightOuterButtonRef = useRef(null);
+
+  //this handler for TAKE TEST button
+  const handleTakeTestHover = () => {
+    //moves text to left
+    gsap.to(sophisticatedRef.current, {
+      x: -(window.innerWidth / 2) + 240, //moves left by 40% of viewport width
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(skincareRef.current, {
+      x: -(window.innerWidth / 2) + 165,
+      duration: 1,
+      ease: "power3.out",
+    });
+    //move DISCOVER AI off the screen
+    gsap.to(leftGroupRef.current, {
+      x: "-50vw",
+      duration: 0.7,
+      ease: "power3.out",
+    });
+
+    // Expand right diamond
+    gsap.to(rightOuterDiamondRef.current, {
+      scale: 1,
+      opacity: 1,
+      duration: 1,
+      ease: "power2.out",
+    });
+    gsap.to(rightInnerDiamondRef.current, {
+      scale: 1,
+      opacity: 1,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
+  const handleTakeTestLeave = () => {
+    gsap.to(sophisticatedRef.current, {
+      x: 0, //moves left by 40% of viewport width
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(skincareRef.current, {
+      x: 0, //moves left by 40% of viewport width
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(leftGroupRef.current, {
+      x: "0vw",
+      duration: 1,
+      ease: "power3.out",
+    });
+
+    // Contract right diamond
+    gsap.to(rightOuterDiamondRef.current, {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+    gsap.to(rightInnerDiamondRef.current, {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
+  //this is handler for  DISCOVER AI button
+  const handleDiscoverAIHover = () => {
+    //moves text to right
+    gsap.to(sophisticatedRef.current, {
+      x: window.innerWidth / 2 - 240, //moves left by 40% of viewport width
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(skincareRef.current, {
+      x: window.innerWidth / 2 - 160,
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(rightGroupRef.current, {
+      x: "50vw",
+      duration: 1,
+      ease: "power3.out",
+    });
+
+    // Expand left diamond
+    gsap.to(leftOuterDiamondRef.current, {
+      scale: 1,
+      opacity: 1,
+      duration: 1,
+      ease: "power2.out",
+    });
+    gsap.to(leftInnerDiamondRef.current, {
+      scale: 1,
+      opacity: 1,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
+  const handleDiscoverAILeave = () => {
+    gsap.to(sophisticatedRef.current, {
+      x: 0, //moves left by 40% of viewport width
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(skincareRef.current, {
+      x: 0,
+      duration: 1,
+      ease: "power3.out",
+    });
+    gsap.to(rightGroupRef.current, {
+      x: "0vw",
+      duration: 1,
+      ease: "power3.out",
+    });
+
+    // Contract left diamond
+    gsap.to(leftOuterDiamondRef.current, {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+    gsap.to(leftInnerDiamondRef.current, {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
   return (
-    <div className="relative h-screen">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="text-[86px] font-semi-semi-bold text-center leading-[1]">
+    <div className="relative h-screen overflow-hidden">
+      {/* left diamond */}
+      <div>
+        <div
+          ref={leftGroupRef}
+          className="absolute inset-y-0 left-0 flex items-center h-full z-10"
+        >
+          {/* Diamond */}
+          <div className="relative">
+            <div
+              className="absolute top-1/2 left-0 w-[360px] h-[360px] border-dotted border-[1.5px] border-gray-400 transform rotate-45 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+            ></div>
+            <div
+              ref={leftInnerDiamondRef}
+              className="absolute top-1/2 left-0 w-[400px] h-[400px] border-dotted border-[1.5px] border-[#b7bcc5] transform rotate-45 -translate-x-1/2 -translate-y-1/2 pointer-events-none opacity-0 scale-80"
+            ></div>
+            <div
+              ref={leftOuterDiamondRef}
+              className="absolute top-1/2 left-0 w-[440px] h-[440px] border-dotted border-[1.5px] border-gray-300 transform rotate-45 -translate-x-1/2 -translate-y-1/2 pointer-events-none opacity-0 scale-80"
+            ></div>
+          </div>
+          {/* Button group */}
+          <div
+            className="flex items-center gap-5 ml-10"
+            onMouseEnter={handleDiscoverAIHover}
+            onMouseLeave={handleDiscoverAILeave}
+          >
+            <div
+              className="relative cursor-pointer"
+              onClick={() => router.push("/about")}
+            >
+              {/* rotated box */}
+              <div className="w-8 h-8 border-[0.5px] border-black rotate-45 bg-white"></div>
+              {/* centered triangle */}
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="w-0 h-0 border-t-[5px] border-b-[5px] border-r-[8px] border-t-transparent border-b-transparent border-r-black -translate-x-0.5"></div>
+              </div>
+            </div>
+            <button
+              className="text-[9px] cursor-pointer bg-transparent border-none outline-none"
+              onClick={() => router.push("/about")}
+              type="button"
+            >
+              DISCOVER A.I
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        ref={rightGroupRef}
+        className="absolute inset-y-0 right-0 flex items-center h-full z-10"
+      >
+        {/* Diamond */}
+        <div className="absolute top-1/2 right-0 w-[360px] h-[360px] 
+        border-dotted border-[1.5px] border-gray-400 transform rotate-45 
+        translate-x-1/2 -translate-y-1/2 pointer-events-none"></div>
+        <div
+          ref={rightInnerDiamondRef}
+          className="absolute top-1/2 right-0 w-[400px] h-[400px] border-dotted border-[1.5px] border-[#b7bcc5] transform rotate-45 translate-x-1/2 -translate-y-1/2 pointer-events-none opacity-0 scale-80"
+        ></div>
+        <div
+          ref={rightOuterDiamondRef}
+          className="absolute top-1/2 right-0 w-[440px] h-[440px] border-dotted border-[1.5px] border-gray-300 transform rotate-45 translate-x-1/2 -translate-y-1/2 pointer-events-none opacity-0 scale-80"
+        ></div>
+
+        {/* Button group */}
+        <div
+          className="flex items-center gap-5 mr-10 z-10"
+          onMouseEnter={handleTakeTestHover}
+          onMouseLeave={handleTakeTestLeave}
+        >
+          <button
+            className="text-[9px] cursor-pointer bg-transparent border-none outline-none"
+            onClick={() => router.push("/testing")}
+            type="button"
+          >
+            TAKE TEST
+          </button>
+          <div
+            className="relative cursor-pointer"
+            onClick={() => router.push("/testing")}
+          >
+            {/* rotated box */}
+            <div className="w-8 h-8 border-[0.5px] border-black -rotate-45 bg-white"></div>
+            {/* centered triangle */}
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <div className="w-0 h-0 border-t-[5px] border-b-[5px] border-l-[8px] border-t-transparent border-b-transparent border-l-black translate-x-0.5"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div
+          ref={sophisticatedRef}
+          className="text-[76px] font-normal text-center leading-[1] tracking-[-0.04em]"
+        >
           Sophisticated
-          <br />
+        </div>
+
+        <div
+          ref={skincareRef}
+          className="text-[76px] font-normal text-center leading-[1] tracking-[-0.04em]"
+        >
           skincare
         </div>
       </div>
-      <div className="absolute inset-y-0 left-0 flex items-center cursor-pointer">
-        <button className="ml-12 text-xs" onClick={() => router.push("/about")}>DISCOVER A.I</button>
-      </div>
-      <div className="absolute inset-y-0 right-0 flex items-center">
-        <button
-          className="mr-12 text-xs cursor-pointer"
-          onClick={() => router.push("/testing")}
-        >
-          TAKE TEST
-        </button>
+
+      <div className="absolute bottom-8 left-8 max-w-xs z-10">
+        <p className="text-xs leading-5.5">
+          SKINSTRIC DEVELOPED AN A.I. THAT CREATES A<br />
+          HIGHLY-PERSONALIZED ROUTINE TAILORED TO
+          <br />
+          WHAT YOUR SKIN NEEDS.
+        </p>
       </div>
     </div>
   );
